52fa84570dd38a0d20f3e40565b90fa85460043f
Paul Ennemoser <tivtag@gmail.com>
2014-02-12 21:38:49 +0100

    Enabled Immersive Fullscreen Mode on Android 4.4 (Kitkat)+ devices

---
+++ Assets/Plugins/Android/src/AndroidPlugin.java
@@ -0,0 +1,39 @@
+package com.federrot.lstdt;
+
+import com.unity3d.player.UnityPlayerActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Config;
+import android.util.Log;
+import android.view.View;
+
+public class AndroidPlugin extends UnityPlayerActivity
+{
+	@Override
+	protected void onCreate(Bundle myBundle) {
+		super.onCreate(myBundle);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		if (android.os.Build.VERSION.SDK_INT >= 19) { // KITKAT
+			int flags = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+			this.findViewById(android.R.id.content).setSystemUiVisibility(flags);
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+}
